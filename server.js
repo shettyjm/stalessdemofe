8267c4146deccb724669cd33d4c717f1cbb5195e
Jag <shettyjm@gmail.com>
2023-11-10 10:42:16 -0800

    introduced a tempalted html

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const backendApiUrl = process.env.BACKEND_API_URL || 'https://voteapp.yourmockorrealapi.cloud';
 
-
+app.set('view engine', 'ejs'); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+ // res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.render('index', { backendApiUrl });
 });
 
 app.listen(port, () => {
